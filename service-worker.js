ec0036821d4dcbea26495955a1a87c4ce1be0e97
liz2020 <lihaozhu2020@gmail.com>
2019-06-24 23:53:49 -0400

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/me/precache-manifest.9e0565d5ccd88475db88887bcbb356ec.js"
+  "/me/precache-manifest.c30ca4a87c7744e3457a51030d490bf3.js"
 );
 
 workbox.clientsClaim();
